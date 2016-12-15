932d084d26a0aa479256aa9aa6903c46a7ff98bb
Stef <stef@192.168.2.12>
2016-12-15 16:41:00 +0100

    Comment results.py file

--- results.py
+++ results.py
@@ -2,15 +2,24 @@ class Results:
 
 	def __init__(self, filename):
 		self.filename = filename
+		# Generate an empty array to save the times a player has passed a tile
 		self.hits = [0] * 41
 
 	def addHitResults(self, hits):
+		# Add hits given to the local hits array
 		for i in range(0, 41):
 			self.hits[i] += hits[i]
 
 	def write(self):
+		# Open the file
 		file = open(self.filename, "w")
+		
+		# Write a title and the sum of all the tile hits
 		file.write("Hit frequency;\"=SUM(b2:b42)\";\n")
+		
+		# Write the tile index, the hit count and the chance
 		for i in range(0, len(self.hits)):
 			file.write("%i;%i;\"=b%i/b$1\"\n" % (i, self.hits[i], i + 2))
+
+		# Close and write the file
 		file.close()
